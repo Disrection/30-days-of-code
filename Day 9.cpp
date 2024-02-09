3a4038ccecabdd5accce898b796374c448462725
Disrection <morfmrof@gmail.com>
2024-02-09 20:12:12 +0530

    Day 9 of 30 days of code completed

--- Day 9.cpp
+++ Day 9.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main() {
     int i;
-    int arr[] = {1, 3, 5, 7, 9};
+    int arr[] = {1, 2, 8, 7, 9};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 5;
+    int x = 7;
     for (i = 0; i < n; i++) {
     if (arr[i] == x) {
       cout << "Element found at index " << i << endl;
