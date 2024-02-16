6b201c0c75388631b22ef84a66f0447cc760d19a
Disrection <morfmrof@gmail.com>
2024-02-16 23:13:42 +0530

    Day 16 of 30 days of code completed

---
+++ Day 16 .cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    cin>>input;
+    reverse(input.begin(), input.end());
+    cout << "Reversed string: " << input << endl;
+    return 0;
+}
